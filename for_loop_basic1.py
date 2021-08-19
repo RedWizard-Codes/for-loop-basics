e0cda403150bfcdf4e2cd2eb7263aa3be1bce923
RedWizard-Codes <89153034+RedWizard-Codes@users.noreply.github.com>
2021-08-19 18:25:51 -0400

    Add files via upload
    testing Git hub

---
+++ for_loop_basic1.py
@@ -0,0 +1,28 @@
+for x in range(0,150): 
+    print(x)
+
+for x in range(0,1000):
+    if x % 5==0:
+        print(x)
+
+for x in range(1,100):
+    if x % 5==0:
+        print("Coding")
+    if x % 10==0:
+        print("Dojo")
+    else: print(x)
+
+sum=0
+for x in range(0,500000):
+    if x % 2!=0:
+        print(sum)
+        sum=x+sum
+
+for x in range(2018,0,-4):
+    print(x)
+
+low_num=1
+high_num=32
+int=5
+for x in range(low_num, high_num, int):
+    print(x)
